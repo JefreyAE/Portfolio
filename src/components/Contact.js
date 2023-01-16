939b32d4cb9cbf5418782bd3be595977fcf3000d
Jefrey Arias Esquivel <jefariasj@gmail.com>
2023-01-16 15:45:27 -0600

    Added social networks

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,13 +4,24 @@ export const Contact = () => {
   return (
     <div className='page'>
       <h1 className='heading'>Contact</h1>
-        <form className='contact' action='mailto:jefariasj@gmail.com'>
-          <input type='text' placeholder='First name'/>
-          <input type='text' placeholder='Last name'/>
-          <input type='email' placeholder='Email'/>
-          <textarea placeholder='Reason for contact'></textarea>
-          <input type='submit' value='Send' />
-        </form>
+        <div className='contact-container'>
+          <section className='contact-section'>
+          <h2>Send an email</h2>
+            <form className='contact' action='mailto:jefariasj@gmail.com'>
+              <input type='text' placeholder='First name'/>
+              <input type='text' placeholder='Last name'/>
+              <input type='email' placeholder='Email'/>
+              <textarea placeholder='Reason for contact'></textarea>
+              <input type='submit' value='Send' />
+            </form>
+          </section>
+          <section className='contact-section'>
+            <h2>Social Networks</h2>
+            <div  className='social-net'>
+            <a href='https://www.linkedin.com/in/jefreyariasesquivel-cr/' target='_blank' rel='noreferrer'><img src='/images/linkedIn.png'/></a><a href='https://www.linkedin.com/in/jefreyariasesquivel-cr/' target='_blank' rel='noreferrer'>jefreyariasesquivel-cr</a>
+            </div>
+          </section>
+        </div>
     </div>
   )
 }
